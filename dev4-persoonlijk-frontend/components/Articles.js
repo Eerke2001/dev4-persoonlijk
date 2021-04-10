ae6faa8994e5235d66a85ec7f42f299eba980a1e
Eerke2001 <erin.troch@student.howest.be>
2021-04-10 10:44:23 +0200

    articles fix errors

---
+++ dev4-persoonlijk-frontend/components/Articles.js
@@ -0,0 +1,19 @@
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+
+const Articles = ({ articles = [] }) => {
+    return (
+        <div className={styles.grid}>
+            {articles.map((article) => (
+                <Link key={article.id} href={`/articles/${article.slug}`}>
+                    <a className={styles.card}>
+                        <h3>{article.title}</h3>
+                        <p>{article.description}</p>
+                    </a>
+                </Link>
+            ))}
+        </div>
+    );
+};
+
+export default Articles;
--- dev4-persoonlijk-frontend/pages/index.js
+++ dev4-persoonlijk-frontend/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import Layout from "../components/Layout";
+import Articles from "../components/Articles.js";
 import styles from "../styles/Home.module.css";
 import AddArticle from "../components/AddArticle.js";
 import { useState } from "react";
@@ -24,7 +25,8 @@ export default function Home({ data }) {
     );
     if (response.ok) {
       const result = await response.json();
-      const tmp = [...articles, result];
+      console.log(result);
+      const tmp = [result, ...articles];
       console.log(tmp);
       setArticles(tmp);
     }
@@ -35,16 +37,7 @@ export default function Home({ data }) {
     <Layout>
       <AddArticle onSubmit={handleSubmit}></AddArticle>
       <p>{process.env.NEXT_PUBLIC_DEMO}</p>
-      <div className={styles.grid}>
-        {data.map((article) => (
-          <Link key={article.id} href={`/articles/${article.slug}`}>
-            <a className={styles.card}>
-              <h3>{article.title}</h3>
-              <p>{article.description}</p>
-            </a>
-          </Link>
-        ))}
-      </div>
+      <Articles articles={articles} />
     </Layout>
   );
 }
