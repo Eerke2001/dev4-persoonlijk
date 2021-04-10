ed46e9c1b51098ad26eb9c0c13056f33e40e2a6b
Eerke2001 <erin.troch@student.howest.be>
2021-04-10 21:01:05 +0200

    try auto scroll

--- dev4-persoonlijk-frontend/components/Comments.js
+++ dev4-persoonlijk-frontend/components/Comments.js
@@ -1,17 +1,42 @@
 import styles from "./Comments.module.css";
+import React, { useEffect, useRef, useState } from 'react';
 
 const Comments = ({ comments = [] }) => {
+
+  const messagesRef = useRef(null);
+  const scrollToBottom = () => {
+    messagesRef.current.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "start"
+    });
+  };
+
+  useEffect(() => {
+    if (messagesRef.current) {
+      console.log(messagesRef.current);
+      scrollToBottom();
+    }
+  }, []);
+
   return (
     <>
       <h3 className={styles.title}>Chat</h3>
-      <section className={styles.messages}>
+      <section className={styles.messages} id="ul">
         <ul className={styles.ul}>
-          {comments.map((comment) => (
-            <li className={styles.li} key={comment.id}>
-              <strong>{comment.name}</strong>
-              <p>{comment.content}</p>
-            </li>
-          ))}
+          {
+            comments.map((comment) => (
+              comments.indexOf(comment) !== comments.length - 1 ?
+                <li className={styles.li} key={comment.id}>
+                  <p className={styles.liName}>{comment.name}</p>
+                  <p className={styles.p}>{comment.content}</p>
+                </li> :
+                <li ref={messagesRef} className={styles.li} key={comment.id}>
+                  <p className={styles.liName}>{comment.name}</p>
+                  <p className={styles.p}>{comment.content}</p>
+                </li>
+            ))
+          }
         </ul>
       </section>
     </>
