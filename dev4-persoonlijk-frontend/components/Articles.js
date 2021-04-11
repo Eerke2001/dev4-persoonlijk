d06ec8d11879f9e5d5bef2800a31b1751992cffd
Eerke2001 <erin.troch@student.howest.be>
2021-04-11 13:07:32 +0200

    postits

--- dev4-persoonlijk-frontend/components/Articles.js
+++ dev4-persoonlijk-frontend/components/Articles.js
@@ -2,12 +2,16 @@ import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
 const Articles = ({ articles = [] }) => {
+
+    const colorArray = [`#9cffbe`, `#ff85ad`, `#f4b5ff`, `#9cf3ff`, `#96b6ff`];
+
     return (
-        <div className={styles.grid}>
+        <div className={styles.cardDisplay}>
             {articles.map((article) => (
                 <Link key={article.id} href={`/articles/${article.slug}`}>
-                    <a className={styles.card}>
-                        <h3>{article.title} - <span className={styles.sender}>{article.sender}</span></h3>
+                    <a style={{ backgroundColor: colorArray[Math.floor(Math.random() * 5)], marginTop: `${Math.floor(Math.random() * 20)}px` }} className={styles.card}>
+                        <h3 className={styles.title}>{article.title}</h3>
+                        <p className={styles.sender}>- {article.sender}</p>
                         <p>{article.description}</p>
                     </a>
                 </Link>
